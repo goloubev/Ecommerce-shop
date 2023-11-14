a871bdca413753e28021d7d739b6fdd322184518
Vadim Goloubev <goloubev@hotmail.com>
2023-11-14 17:16:25 -0500

    Commit

--- app/Http/Controllers/Admin/API/Product/IndexController.php
+++ app/Http/Controllers/Admin/API/Product/IndexController.php
@@ -3,18 +3,31 @@
 namespace App\Http\Controllers\Admin\API\Product;
 
 use App\Http\Controllers\Controller;
+use App\Http\Filters\ProductFilter;
+use App\Http\Requests\API\Product\IndexRequest;
 use App\Http\Resources\Product\IndexProductResource;
 use App\Models\Product;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class IndexController extends Controller
 {
-    public function __invoke(): AnonymousResourceCollection
+    /**
+     * @throws BindingResolutionException
+     */
+    public function __invoke(IndexRequest $request): AnonymousResourceCollection
     {
+        $data = $request->validated();
+
+        $filter = app()->make(ProductFilter::class, [
+            'queryParams' => array_filter($data)
+        ]);
+
         // php artisan make:resource Product/ProductResource
         // php artisan make:resource Category/CategoryResource
-        $products = Product::all();
+        $products = Product::filter($filter)->get();
+        $result = IndexProductResource::collection($products);
 
-        return IndexProductResource::collection($products);
+        return $result;
     }
 }
