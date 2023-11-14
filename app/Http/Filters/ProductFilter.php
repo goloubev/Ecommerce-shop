4366b72e85d6f97f9e9de4ee68897d78529f9b4e
Vadim Goloubev <goloubev@hotmail.com>
2023-11-14 18:17:02 -0500

    Commit

--- app/Http/Filters/ProductFilter.php
+++ app/Http/Filters/ProductFilter.php
@@ -8,40 +8,55 @@ class ProductFilter extends AbstractFilter
 {
     const CATEGORIES = 'categories';
     const COLORS = 'colors';
-    const PRICE = 'price';
     const TAGS = 'tags';
+    const PRICES = 'prices';
+    const ORDER = 'order';
 
     protected function getCallbacks(): array
     {
         $callbacks = [
             self::CATEGORIES => [$this, 'categories'],
             self::COLORS => [$this, 'colors'],
-            self::PRICE => [$this, 'price'],
             self::TAGS => [$this, 'tags'],
+            self::PRICES => [$this, 'prices'],
+            self::ORDER => [$this, 'order'],
         ];
 
         return $callbacks;
     }
 
-    protected function categories(Builder $builder, $value)
+    protected function categories(Builder $builder, $value): void
     {
         $builder->whereIn('category_id', $value);
     }
 
-    protected function colors(Builder $builder, $value)
+    protected function colors(Builder $builder, $value): void
     {
-        $builder->whereIn('color_id', $value);
+        $builder->whereHas('colors', function ($b) use ($value) {
+            $b->whereIn('color_id', $value);
+        });
     }
 
-    protected function price(Builder $builder, $value)
+    protected function tags(Builder $builder, $value): void
     {
-        $builder->whereBetween($value['from'], $value['to']);
-    }
-
-    protected function tags(Builder $builder, $value)
-    {
-        $builder->whereHas('tags', function($b) use ($value) {
+        $builder->whereHas('tags', function ($b) use ($value) {
             $b->whereIn('tag_id', $value);
         });
+    }
+
+    protected function prices(Builder $builder, $value): void
+    {
+        $builder->whereBetween('price', $value);
+    }
+
+    protected function order(Builder $builder, $value): void
+    {
+        $acceptedFields = ['title', 'price'];
+        $acceptedSorts = ['asc', 'desc'];
+        [$fieldName, $sortOrder] = explode('|', $value, 2);
+
+        if (in_arrayw($fieldName, $acceptedFields) && in_array(strToLower($sortOrder), $acceptedSorts)) {
+            $builder->orderBy($fieldName, $sortOrder);
+        }
     }
 }
--- app/Http/Requests/API/Product/IndexRequest.php
+++ app/Http/Requests/API/Product/IndexRequest.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Http\Requests\Admin\Product;
+namespace App\Http\Requests\API\Product;
 
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreRequest extends FormRequest
+class IndexRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -23,18 +23,11 @@ class StoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title' => ['required', 'string', 'min:3', 'max:255'],
-            'description' => ['required', 'string'],
-            'content' => ['required', 'string'],
-            'product_images' => ['nullable', 'array'],
-            'price' => ['required', 'decimal:0,2'],
-            'price_old' => ['nullable', 'decimal:0,2'],
-            'count' => ['required', 'integer', 'min:1', 'max:9999'],
-            'is_published' => ['required', 'boolean'],
-            'category_id' => ['nullable'],
-            'group_id' => ['nullable'],
-            'tag_ids' => ['nullable', 'array'],
-            'color_ids' => ['nullable', 'array'],
+            'categories' => ['nullable', 'array'],
+            'colors' => ['nullable', 'array'],
+            'tags' => ['nullable', 'array'],
+            'prices' => ['nullable', 'array'],
+            'order' => ['nullable', 'string'],
         ];
     }
 }
